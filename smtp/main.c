e63352cfb0e462300547637f1af19b729f74b8ad
Ruchi Bhalchandra Bhogate <rbhogate@silo.luddy.indiana.edu>
2023-09-05 23:21:03 -0400

    smtp task 2 with comments

--- smtp/main.c
+++ smtp/main.c
@@ -24,34 +24,50 @@ int main(int argc, char* argv[]) {
   /* 
      STUDENT CODE HERE
    */
-  
-  int socket = connect_smtp("lunar.open.sice.indiana.edu", 25);
+
   char response[4096];
+  //Starting the connection to the SMTP server
+  int socket = connect_smtp("lunar.open.sice.indiana.edu", 25);
+  //Sending message to the server and getting back the response
   send_smtp(socket, "HELO iu.edu\r\n", response, 4096);
+  //Printing the response of the server
   printf("%s\n", response);
+
   char msg1[100];
   strcpy(msg1,"MAIL FROM:");
   strcat(msg1,rcpt);
   strcat(msg1,"\r\n");
   send_smtp(socket, msg1, response, 4096);
   printf("%s\n", response);
+
   char msg2[100];
   strcpy(msg2,"RCPT TO:");
   strcat(msg2,rcpt);
   strcat(msg2,"\r\n");
   send_smtp(socket, msg2, response, 4096);
   printf("%s\n", response);
+
   send_smtp(socket, "DATA\r\n", response, 4096);
   printf("%s\n", response);
-  FILE *fptr;
-  fptr = fopen(filepath, "r");
-  char s1[100];
-  char email[5000];
-  while(fgets(s1,100,fptr)){
-    strcat(email,s1);
+
+  //Reading the txt file that contains email data
+  FILE *f;
+  f = fopen(filepath, "r");
+  char s[100];
+  char email[4096];
+
+  //To clear the character array before storing the file content
+  for(int i=0; i<4096; i++){
+    email[i] = '\0';
   }
-  fclose(fptr);
+
+  while(fgets(s,100,f)){
+    strcat(email,s);
+  }
+  fclose(f);
+  //Adding trailing period to complete the exchange
   strcat(email,"\r\n.\r\n");
+
   send_smtp(socket, email, response, 4096);
   printf("%s\n", response);
 
