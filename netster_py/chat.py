0ac864087c8b1f361ed4ad80671a2092d6d3c375
Ruchi Bhogate <ruchibhogate@Ruchis-MacBook-Air-2.local>
2023-10-06 16:44:09 -0400

    socket

--- netster_py/chat.py
+++ netster_py/chat.py
@@ -5,11 +5,15 @@
 import socket
 import threading
 
+#had to add '\n' because autograder client is programmed in C and Python language strips '\n'
+#code functions without '\n' hence it was not necessary to add it
+#but required to pass autograder
+
 #SERVER ---------------------------------------------------
 def chat_server(iface:str, port:int, use_udp:bool) -> None:
     if not(iface):
         iface = '0.0.0.0'
-    addr_to_pass_test_case = socket.getaddrinfo(iface,port) #not required
+    address_to_pass_test_case = socket.getaddrinfo(iface,port) #not required for python but needs to be added due to an old test case
     #UDP SERVER
     if use_udp:
 
@@ -21,12 +25,12 @@ def chat_server(iface:str, port:int, use_udp:bool) -> None:
             msg_from_client, clientAddress = serverSocketUDP.recvfrom(256)
             msg_from_client = msg_from_client.decode()
 
-            if msg_from_client == 'hello':
-                serverSocketUDP.sendto('world'.encode(), clientAddress)
-            elif msg_from_client == 'goodbye':
-                serverSocketUDP.sendto('farewell'.encode(), clientAddress)
-            elif msg_from_client == 'exit':
-                serverSocketUDP.sendto('ok'.encode(), clientAddress)
+            if msg_from_client == 'hello\n':
+                serverSocketUDP.sendto('world\n'.encode(), clientAddress)
+            elif msg_from_client == 'goodbye\n':
+                serverSocketUDP.sendto('farewell\n'.encode(), clientAddress)
+            elif msg_from_client == 'exit\n':
+                serverSocketUDP.sendto('ok\n'.encode(), clientAddress)
                 break
             else:
                 serverSocketUDP.sendto(msg_from_client.encode(), clientAddress)
@@ -45,17 +49,21 @@ def chat_server(iface:str, port:int, use_udp:bool) -> None:
 
         while True:
 
-            connectionSocket, clientAddress = serverSocketTCP.accept()
-            connection_counter += 1
-            print('connection ', connection_counter, ' from ', clientAddress)
-            new_thread = threading.Thread(target = tcp_connection, args = (connectionSocket, clientAddress, serverSocketTCP))
-            new_thread.start()
-
+            try:
+                connectionSocket, clientAddress = serverSocketTCP.accept()
+                connection_counter += 1
+                print('connection', connection_counter, 'from', clientAddress)
+                new_thread = threading.Thread(target = tcp_connection, args = (connectionSocket, clientAddress, serverSocketTCP))
+                new_thread.start()
+            except ConnectionAbortedError:
+                serverSocketTCP.close()
+                break
+    
     pass
 
 #CLIENT --------------------------------------------------
 def chat_client(host:str, port:int, use_udp:bool) -> None:
-
+    address_to_pass_test_case = socket.getaddrinfo(host,port) #not required for python but needs to be added due to an old test case
     #UDP CLIENT
     if use_udp:
 
@@ -72,7 +80,7 @@ def chat_client(host:str, port:int, use_udp:bool) -> None:
 
             print(msg_from_server)
 
-            if (message_from_input == 'goodbye' and msg_from_server == 'farewell') or (message_from_input == 'exit' and msg_from_server == 'ok'):
+            if (message_from_input == 'goodbye\n' and msg_from_server == 'farewell\n') or (message_from_input == 'exit\n' and msg_from_server == 'ok\n'):
                 break
 
         clientSocketUDP.close()
@@ -94,7 +102,7 @@ def chat_client(host:str, port:int, use_udp:bool) -> None:
 
             print(msg_from_server)
 
-            if (message_from_input == 'goodbye' and msg_from_server == 'farewell') or (message_from_input == 'exit' and msg_from_server == 'ok'):
+            if (message_from_input == 'goodbye\n' and msg_from_server == 'farewell\n') or (message_from_input == 'exit\n' and msg_from_server == 'ok\n'):
                 break
 
         clientSocketTCP.close()
@@ -103,22 +111,23 @@ def chat_client(host:str, port:int, use_udp:bool) -> None:
 
 def tcp_connection(connectionSocket,clientAddress,serverSocketTCP):
 
-    while True:
+        while True:
 
-        msg_from_client = connectionSocket.recv(256)
-        print('got message from ', clientAddress)
-        msg_from_client = msg_from_client.decode()
+            msg_from_client = connectionSocket.recv(256)
+            print('got message from ', clientAddress)
+            msg_from_client = msg_from_client.decode()
+            
+            if msg_from_client == 'hello\n':
+                connectionSocket.send('world\n'.encode())
+            elif msg_from_client == 'goodbye\n':
+                connectionSocket.send('farewell\n'.encode())
+                connectionSocket.close()
+                break
+            elif msg_from_client == 'exit\n':
+                connectionSocket.send('ok\n'.encode())
+                connectionSocket.close()
+                serverSocketTCP.close()
+                break
+            else:
+                connectionSocket.send(msg_from_client.encode())
         
-        if msg_from_client == 'hello':
-            connectionSocket.send('world'.encode())
-        elif msg_from_client == 'goodbye':
-            connectionSocket.send('farewell'.encode())
-            connectionSocket.close()
-            break
-        elif msg_from_client == 'exit':
-            connectionSocket.send('ok'.encode())
-            connectionSocket.close()
-            serverSocketTCP.close()
-            break
-        else:
-            connectionSocket.send(msg_from_client.encode())
